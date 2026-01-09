1d53dd3a2d85496f656c03bbef220efb2f4a34ee
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-01-09 06:30:09 +0530

    Add files via upload

---
+++ Task_Remainder_Application/src/main/java/com/project/enums/TaskPriority.java
@@ -0,0 +1,9 @@
+package com.project.enums;
+
+public enum TaskPriority {
+
+    LOW,
+    MEDIUM,
+    HIGH
+
+}
---
+++ Task_Remainder_Application/src/main/java/com/project/enums/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.project.enums;
+
+public enum TaskStatus {
+
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+
+}
